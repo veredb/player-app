f1a172ca4a2d3ad5021adc080ee51af0d9b93ecb
vered bauer <veredbr2000@yahoo.com>
2013-02-07 10:55:06 -0600

    update

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,4 @@ class Player < ActiveRecord::Base
   attr_accessible :birthYear, :nameFirst, :nameLast, :playerID
 
   validates :nameFirst, :nameLast, :birthYear, :playerID, :presence => true
-  validates :playerID, :uniqueness => { :case_sensitive => false }
 end
